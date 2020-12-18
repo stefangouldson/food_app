16c6b6f93477abf0a4237702a48ff0f42beb0388
Stefan Gouldson <sgouldson@CASPIAN71.caspian.local>
2020-12-18 23:38:45 +0000

    initial commit

---
+++ food/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+
+app_name = 'food'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:item_id>/', views.details, name="detail"),
+    path('add', views.create_item, name="create_item"),
+    path('update/<int:id>', views.update_item, name="update_item"),
+    path('delete/<int:id>', views.delete_item, name="delete_item"),
+]
